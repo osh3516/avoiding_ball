9d14af4d12cc81ea01ff73f73cc3158c0e86e62e
osh3516 <ohs5031@naver.com>
2020-09-20 20:15:02 +0900

    avoiding ball

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 10f; 
+    public GameObject explosion;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+  
+    }
+
+    IEnumerator FadeIn()
+    {
+        for (float i = 1f; i >= 0; i -= 0.01f)
+        {
+            Color color = new Vector4(1, 1, 1, i);
+            transform.GetComponent<Renderer>().material.color = color;
+            Destroy(gameObject, 0.7f);
+            yield return 0;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log("Hit");
+     
+        if (other.gameObject.tag =="Ground")
+        {
+            Debug.Log("Hit Ground");
+          
+            StartCoroutine("FadeIn");
+           
+        }
+        else if (other.gameObject.tag == "Player")
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+            gameObject.SetActive(false);
+        }
+    }
+    
+
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+using UnityEngine.SceneManagement; 
+
+public class GameManager : MonoBehaviour {
+    public GameObject gameoverText; 
+    public Text timeText;
+
+    public GameObject bulletPrefab;
+    public float spawnRateMin = 0.5f;
+    public float spawnRateMax = 3f;
+
+    private float surviveTime; 
+    private bool isGameover;
+
+    private float spawnRate; 
+    private float timeAfterSpawn; 
+
+    void Start() {
+        surviveTime = 0;
+        isGameover = false;
+
+       
+        timeAfterSpawn = 0f;
+        spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+    }
+
+    void Update() {
+        if (!isGameover)
+        {
+            timeAfterSpawn += Time.deltaTime;
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time : " + (int)surviveTime;
+         
+            if (timeAfterSpawn >= spawnRate)
+            {
+                timeAfterSpawn = 0f;
+
+                GameObject bullet = Instantiate(bulletPrefab,new Vector3(Random.Range(-8f, 8f), 20f, Random.Range(-120f, 120f)),Quaternion.identity);
+                GameObject bullet2 = Instantiate(bulletPrefab, new Vector3(Random.Range(-8f, 8f), 20f, Random.Range(-120f, 120f)), Quaternion.identity);
+                GameObject bullet3 = Instantiate(bulletPrefab, new Vector3(Random.Range(-8f, 8f), 20f, Random.Range(-120f, 120f)), Quaternion.identity);
+
+                spawnRate = Random.Range(spawnRateMin, spawnRateMax);
+            }
+
+            if (surviveTime > 30)
+            {
+                PlayerController con = FindObjectOfType<PlayerController>();
+                con.gameObject.SetActive(false);
+                EndGame();
+            }
+        }
+       
+    }
+
+    public void EndGame() {
+        isGameover = true;
+       
+        gameoverText.SetActive(true);
+        Application.Quit();
+
+        
+
+
+    }
+}
